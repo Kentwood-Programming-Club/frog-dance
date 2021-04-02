e15288ded2c13a1886c764e27a78d840ccd2c181
Nathaniel D'Rozario <nsdrozario@gmail.com>
2021-04-02 15:34:09 -0700

    looped images

--- auto_fill_table.js
+++ auto_fill_table.js
@@ -6,6 +6,6 @@ let table_rows = document.getElementsByTagName("tr");
 
 for ( let i = 0; i < rows; i++ ) {
     for (let j = 0; j < cols; j++) {
-        table_rows[i].innerHTML += "<td><img src='images/image_" + (i * cols + j + 1) + ".gif'/><td/>"; 
+        table_rows[i].innerHTML += "<td><img src='images/image_" + (((i * cols + j) % 6) + 1) + ".gif'/><td/>"; 
     }
 }
