87e70e1dcd2cacaa3f6914eba480899375723e3d
Nathaniel D'Rozario <nsdrozario@gmail.com>
2021-04-02 15:30:38 -0700

    automatic loading

---
+++ auto_fill_table.js
@@ -0,0 +1,11 @@
+let cols = 4;
+let rows = 4;
+let frog_imgs = 6;
+
+let table_rows = document.getElementsByTagName("tr");
+
+for ( let i = 0; i < rows; i++ ) {
+    for (let j = 0; j < cols; j++) {
+        table_rows[i].innerHTML += "<td><img src='images/image_" + (i * cols + j + 1) + ".gif'/><td/>"; 
+    }
+}
